ba6aeef436927ad634e98396638d6e130e3aebc2
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-01-11 22:09:58 -0300

    Navbar mas carrito

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar/NavBar";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
