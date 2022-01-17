fa9c7ea116ec3d38ee2421a9bdc9e24e255987bb
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-01-17 11:49:06 -0300

    Agregado ItemListContainer-desafio4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import NavBar from "./components/NavBar/NavBar";
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greetings="soy una props" />
     </div>
   );
 }
